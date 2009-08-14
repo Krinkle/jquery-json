496e714b99dabc9d6844f61609111e1a3b7c4ce6
Brantley Harris <deadwisdom@gmail.com>
2009-08-14 15:29:07 +0000

    Complete refactor, support for native JSON.stringify

--- jquery.json.test.js
+++ jquery.json.test.js
@@ -2,10 +2,19 @@
 
 var tests_fail = 0;
 
-function _test(o, correct, compact)
+function str(what)
+{
+    if (what == null)
+        return 'null';
+    if (what == undefined)
+        return 'undefined';
+    return what.toString();
+}
+
+function _test(o, correct)
 {
     try { 
-        var result = $.toJSON(o, compact);
+        var result = $.toJSON(o);
     } catch (e) { 
         if (e.name === correct.name) return;   // For TypeError, etc
         tests_fail += 1;
@@ -13,32 +22,58 @@ function _test(o, correct, compact)
     }
 
     if (result != correct) 
-    {   
+    {
         tests_fail += 1;
-        throw new Error("Wanted: " + correct.toString() + " -- Got: " + result.toString());
+        throw new Error("Wanted: " + str(correct) + " -- Got: " + str(result));
     }
 }
 
 // 'console' comes from FireBug
-function testJSON(o, full, compact) {
-    try { _test(o, full); }
+function testJSON(o, expected) {
+    _test(o, expected);
+    try { _test(o, expected); }
     catch (e) { console.error("Conversion Error:", e.message) }
     
-    if (compact == undefined) return;
-    
-    try { _test(o, compact, true); }
-    catch (e) { console.error("Compact Conversion Error:", e.message) }
+    if (JSON && JSON.stringify)
+    {
+        var _stringify = JSON.stringify;
+        JSON.stringify = null;
+        
+        try { _test(o, expected); }
+        catch (e) { console.error("Manual Conversion Error:", e.message) }
+        
+        JSON.stringify = _stringify;
+    }
 }
 
 console.log("Testing...");
 testJSON('hi', "\"hi\"");
-testJSON({apple: 2}, "{\"apple\": 2}", "{\"apple\":2}");
-testJSON({apple: {apple: 2}}, "{\"apple\": {\"apple\": 2}}", "{\"apple\":{\"apple\":2}}");
+testJSON({apple: 2}, "{\"apple\":2}");
+testJSON({apple: {apple: 2}}, "{\"apple\":{\"apple\":2}}");
 testJSON(2.5, "2.5");
 testJSON(25, "25");
-testJSON(new Date(2008, 9, 25), "2008-09-25");
-testJSON([2, 5], "[2, 5]", "[2,5]");
-testJSON(function() {}, TypeError);
+testJSON([2, 5], "[2,5]");
+testJSON(function() {}, undefined);
+testJSON(function() {}, undefined);
+testJSON(null, "null");
+testJSON(new Date(2008, 9, 25), '"2008-10-25T05:00:00.000Z"');
+
+// Temporarily remove Date's toJSON and JSON.stringify
+dateToJSON = Date.prototype.toJSON;
+Date.prototype.toJSON = null;
+if (JSON && JSON.stringify)
+{
+    jsonStringify = JSON.stringify
+    JSON.stringify = null;
+}
+
+testJSON(new Date(2008, 9, 25), '"2008-10-25T05:00:00.000Z"');
+
+// Restore Date's toJSON and JSON.stringify
+Date.prototype.toJSON = dateToJSON;
+if (jsonStringify)
+    JSON.stringify = jsonStringify;
+
 console.log("Done.")
 
 $(function()

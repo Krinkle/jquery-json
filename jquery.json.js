16258290fcce878bd3f78eb0994f688fc307c94b
Timo Tijhof <krinklemail@gmail.com>
2011-09-17 21:58:53 +0000

    Applying style conventions * Bump version number (home page already advertised 2.2 but was never reflected into the code) * Breaking lines around 80-100 chars * Using tabs instead of spaces * Whitespacing according to http://docs.jquery.com/JQuery_Core_Style_Guidelines#Spacing * Added function documentation, removing redundant parenthesis for typeof (It's not a function but an operator) * Moving var statements higher up to before they are used * Using other best practices, validating JSHint ** Adding missing semicolons ** Strict comparison

--- jquery.json.js
+++ jquery.json.js
@@ -1,178 +1,188 @@
-/*
+/**
  * jQuery JSON Plugin
- * version: 2.1 (2009-08-14)
+ * version: 2.2 (2011-09-16)
  *
  * This document is licensed as free software under the terms of the
  * MIT License: http://www.opensource.org/licenses/mit-license.php
  *
- * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org 
+ * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org
  * website's http://www.json.org/json2.js, which proclaims:
  * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.", a sentiment that
  * I uphold.
  *
- * It is also influenced heavily by MochiKit's serializeJSON, which is 
+ * It is also influenced heavily by MochiKit's serializeJSON, which is
  * copyrighted 2005 by Bob Ippolito.
  */
- 
-(function($) {
-    /** jQuery.toJSON( json-serializble )
-        Converts the given argument into a JSON respresentation.
 
-        If an object has a "toJSON" function, that will be used to get the representation.
-        Non-integer/string keys are skipped in the object, as are keys that point to a function.
+(function( $ ) {
+	/**
+	 * jQuery.toJSON
+	 * Converts the given argument into a JSON respresentation.
+	 *
+	 * @param o {Mixed} The json-serializble *thing* to be converted
+	 *
+	 * If an object has a "toJSON" function, that will be used to get the representation.
+	 * Non-integer/string keys are skipped in the object, as are keys that point to a
+	 * function.
+	 *
+	 */
+	$.toJSON = function( o ) {
+		if ( typeof JSON  === 'object' && JSON.stringify ) {
+			return JSON.stringify( o );
+		}
 
-        json-serializble:
-            The *thing* to be converted.
-     **/
-    $.toJSON = function(o)
-    {
-        if (typeof(JSON) == 'object' && JSON.stringify)
-            return JSON.stringify(o);
-        
-        var type = typeof(o);
-    
-        if (o === null)
-            return "null";
-    
-        if (type == "undefined")
-            return undefined;
-        
-        if (type == "number" || type == "boolean")
-            return o + "";
-    
-        if (type == "string")
-            return $.quoteString(o);
-    
-        if (type == 'object')
-        {
-            if (typeof o.toJSON == "function") 
-                return $.toJSON( o.toJSON() );
-            
-            if (o.constructor === Date)
-            {
-                var month = o.getUTCMonth() + 1;
-                if (month < 10) month = '0' + month;
+		var type = typeof o;
 
-                var day = o.getUTCDate();
-                if (day < 10) day = '0' + day;
+		if ( o === null ) {
+			return 'null';
+		}
+		if ( type === 'undefined' ) {
+			return undefined;
+		}
+		if ( type === 'number' || type === 'boolean' ) {
+			return o + '';
+		}
+		if ( type === 'string') {
+			return $.quoteString( o );
+		}
+		if ( type === 'object' ) {
+			if ( typeof o.toJSON === 'function' ) {
+				return $.toJSON( o.toJSON() );
+			}
+			if ( o.constructor === Date ) {
+				var month = o.getUTCMonth() + 1;
+				if ( month < 10 ) {
+					month = '0' + month;
+				}
+				var day = o.getUTCDate();
+				if ( day < 10 ) {
+					day = '0' + day;
+				}
+				var year = o.getUTCFullYear();
+				var hours = o.getUTCHours();
+				if ( hours < 10 ) {
+					hours = '0' + hours;
+				}
+				var minutes = o.getUTCMinutes();
+				if ( minutes < 10 ) {
+					minutes = '0' + minutes;
+				}
+				var seconds = o.getUTCSeconds();
+				if ( seconds < 10 ) {
+					seconds = '0' + seconds;
+				}
+				var milli = o.getUTCMilliseconds();
+				if ( milli < 100 ) {
+					milli = '0' + milli;
+				}
+				if ( milli < 10 ) {
+					milli = '0' + milli;
+				}
+				return '"' + year + '-' + month + '-' + day + 'T' +
+				hours + ':' + minutes + ':' + seconds +
+				'.' + milli + 'Z"';
+			}
+			if ( o.constructor === Array ) {
+				var ret = [];
+				for ( var i = 0; i < o.length; i++ ) {
+					ret.push( $.toJSON( o[i] ) || 'null' );
+				}
+				return '[' + ret.join(',') + ']';
+			}
+			var pairs = [];
+			for ( var k in o ) {
+				var name;
+				var type = typeof k;
+				if ( type === 'number' ) {
+					name = '"' + k + '"';
+				} else if (type === 'string') {
+					name = $.quoteString(k);
+				} else {
+					//skip non-string or number keys
+					continue;
+				}
 
-                var year = o.getUTCFullYear();
-                
-                var hours = o.getUTCHours();
-                if (hours < 10) hours = '0' + hours;
-                
-                var minutes = o.getUTCMinutes();
-                if (minutes < 10) minutes = '0' + minutes;
-                
-                var seconds = o.getUTCSeconds();
-                if (seconds < 10) seconds = '0' + seconds;
-                
-                var milli = o.getUTCMilliseconds();
-                if (milli < 100) milli = '0' + milli;
-                if (milli < 10) milli = '0' + milli;
+				if ( typeof o[k] === 'function' ) {
+					//skip pairs where the value is a function.
+					continue;
+				}
+				var val = $.toJSON( o[k] );
+				pairs.push(name + ':' + val);
+			}
+			return '{' + pairs.join(', ') + '}';
+		}
+	};
 
-                return '"' + year + '-' + month + '-' + day + 'T' +
-                             hours + ':' + minutes + ':' + seconds + 
-                             '.' + milli + 'Z"'; 
-            }
+	/**
+	 * jQuery.evalJSON
+	 * Evaluates a given piece of json source.
+	 *
+	 * @param src {String}
+	 */
+	$.evalJSON = function( src ) {
+		if ( typeof JSON === 'object' && JSON.parse ) {
+			return JSON.parse( src );
+		}
+		return eval('(' + src + ')');
+	};
 
-            if (o.constructor === Array) 
-            {
-                var ret = [];
-                for (var i = 0; i < o.length; i++)
-                    ret.push( $.toJSON(o[i]) || "null" );
+	/**
+	 * jQuery.secureEvalJSON
+	 * Evals JSON in a way that is *more* secure.
+	 *
+	 * @param src {String}
+	 */
+	$.secureEvalJSON = function( src ) {
+		if ( typeof JSON === 'object' && JSON.parse ) {
+			return JSON.parse( src );
+		}
 
-                return "[" + ret.join(",") + "]";
-            }
-        
-            var pairs = [];
-            for (var k in o) {
-                var name;
-                var type = typeof k;
+		var filtered = src;
+		filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@');
+		filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
+		filtered = filtered.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
 
-                if (type == "number")
-                    name = '"' + k + '"';
-                else if (type == "string")
-                    name = $.quoteString(k);
-                else
-                    continue;  //skip non-string or number keys
-            
-                if (typeof o[k] == "function") 
-                    continue;  //skip pairs where the value is a function.
-            
-                var val = $.toJSON(o[k]);
-            
-                pairs.push(name + ":" + val);
-            }
+		if ( /^[\],:{}\s]*$/.test( filtered ) ) {
+			return eval('(' + src + ')');
+		} else {
+			throw new SyntaxError( 'Error parsing JSON, source is not valid.' );
+		}
+	};
 
-            return "{" + pairs.join(", ") + "}";
-        }
-    };
+	var _escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
+	var _meta = {
+		'\b': '\\b',
+		'\t': '\\t',
+		'\n': '\\n',
+		'\f': '\\f',
+		'\r': '\\r',
+		'"' : '\\"',
+		'\\': '\\\\'
+	};
 
-    /** jQuery.evalJSON(src)
-        Evaluates a given piece of json source.
-     **/
-    $.evalJSON = function(src)
-    {
-        if (typeof(JSON) == 'object' && JSON.parse)
-            return JSON.parse(src);
-        return eval("(" + src + ")");
-    };
-    
-    /** jQuery.secureEvalJSON(src)
-        Evals JSON in a way that is *more* secure.
-    **/
-    $.secureEvalJSON = function(src)
-    {
-        if (typeof(JSON) == 'object' && JSON.parse)
-            return JSON.parse(src);
-        
-        var filtered = src;
-        filtered = filtered.replace(/\\["\\\/bfnrtu]/g, '@');
-        filtered = filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
-        filtered = filtered.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
-        
-        if (/^[\],:{}\s]*$/.test(filtered))
-            return eval("(" + src + ")");
-        else
-            throw new SyntaxError("Error parsing JSON, source is not valid.");
-    };
+	/**
+	 * jQuery.quoteString
+	 * Returns a string-repr of a string, escaping quotes intelligently.
+	 * Mostly a support function for toJSON.
+	 * Examples:
+	 * >>> jQuery.quoteString('apple')
+	 * "apple"
+	 *
+	 * >>> jQuery.quoteString('"Where are we going?", she asked.')
+	 * "\"Where are we going?\", she asked."
+	 */
+	$.quoteString = function( string ) {
+		if ( string.match( _escapeable ) ) {
+			return '"' + string.replace( _escapeable, function( a ) {
+				var c = _meta[a];
+				if ( typeof c === 'string' ) {
+					return c;
+				}
+				c = a.charCodeAt();
+				return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+			}) + '"';
+		}
+		return '"' + string + '"';
+	};
 
-    /** jQuery.quoteString(string)
-        Returns a string-repr of a string, escaping quotes intelligently.  
-        Mostly a support function for toJSON.
-    
-        Examples:
-            >>> jQuery.quoteString("apple")
-            "apple"
-        
-            >>> jQuery.quoteString('"Where are we going?", she asked.')
-            "\"Where are we going?\", she asked."
-     **/
-    $.quoteString = function(string)
-    {
-        if (string.match(_escapeable))
-        {
-            return '"' + string.replace(_escapeable, function (a) 
-            {
-                var c = _meta[a];
-                if (typeof c === 'string') return c;
-                c = a.charCodeAt();
-                return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
-            }) + '"';
-        }
-        return '"' + string + '"';
-    };
-    
-    var _escapeable = /["\\\x00-\x1f\x7f-\x9f]/g;
-    
-    var _meta = {
-        '\b': '\\b',
-        '\t': '\\t',
-        '\n': '\\n',
-        '\f': '\\f',
-        '\r': '\\r',
-        '"' : '\\"',
-        '\\': '\\\\'
-    };
-})(jQuery);
+})( jQuery );
--- jquery.json.test.js
+++ jquery.json.test.js
@@ -1,62 +1,58 @@
-/// Tests ///
+/** Tests **/
 
 var tests_fail = 0;
 
-function str(what)
-{
-    if (what == null)
-        return 'null';
-    if (what == undefined)
-        return 'undefined';
-    return what.toString();
+function str(what) {
+	if (what === null)
+		return 'null';
+	if (what === undefined)
+		return 'undefined';
+	return what.toString();
 }
 
-function _test(o, correct)
-{
-    try { 
-        var result = $.toJSON(o);
-    } catch (e) { 
-        if (e.name === correct.name) return;   // For TypeError, etc
-        tests_fail += 1;
-        throw e;
-    }
+function _test(o, correct) {
+	try { 
+		var result = $.toJSON(o);
+	} catch (e) { 
+		if (e.name === correct.name) return; // For TypeError, etc
+		tests_fail += 1;
+		throw e;
+	}
 
-    if (result != correct) 
-    {
-        tests_fail += 1;
-        throw new Error("Wanted: " + str(correct) + " -- Got: " + str(result));
-    }
+	if (result !== correct) {
+		tests_fail += 1;
+		throw new Error('Wanted: ' + str(correct) + ' -- Got: ' + str(result));
+	}
 }
 
 // 'console' comes from FireBug
 function testJSON(o, expected) {
-    try { _test(o, expected); }
-    catch (e) { console.error("Conversion Error:", e.message) }
-    
-    if (typeof(JSON) == 'object' && JSON.stringify)
-    {
-        var _stringify = JSON.stringify;
-        JSON.stringify = null;
-        
-        try { _test(o, expected); }
-        catch (e) { console.error("Manual Conversion Error:", e.message) }
-        
-        JSON.stringify = _stringify;
-    }
+	try { _test(o, expected); }
+	catch (e) { console.error('Conversion Error:', e.message); }
+	
+	if (typeof JSON === 'object' && JSON.stringify) {
+		var _stringify = JSON.stringify;
+		JSON.stringify = null;
+		
+		try { _test(o, expected); }
+		catch (e) { console.error('Manual Conversion Error:', e.message); }
+		
+		JSON.stringify = _stringify;
+	}
 }
 
-console.log("Testing...");
-testJSON('hi', "\"hi\"");
-testJSON({apple: 2}, "{\"apple\":2}");
-testJSON({apple: {apple: 2}}, "{\"apple\":{\"apple\":2}}");
-testJSON(2.5, "2.5");
-testJSON(25, "25");
-testJSON([2, 5], "[2,5]");
+console.log('Testing...');
+testJSON('hi', '"hi"');
+testJSON({apple: 2}, '{"apple":2}');
+testJSON({apple: {apple: 2}}, '{"apple":{"apple":2}}');
+testJSON(2.5, '2.5');
+testJSON(25, '25');
+testJSON([2, 5], '[2,5]');
 testJSON(function() {}, undefined);
-testJSON("C:\\A.TXT", '"C:\\\\A.TXT"');
-testJSON("C:\\B.TXT", '"C:\\\\B.TXT"');
-testJSON(["C:\\A.TXT","C:\\B.TXT","C:\\C.TXT","C:\\D.TXT"], '["C:\\\\A.TXT","C:\\\\B.TXT","C:\\\\C.TXT","C:\\\\D.TXT"]');
-testJSON({"dDefault": "1.84467440737E+19"}, '{"dDefault":"1.84467440737E+19"}');
+testJSON('C:\\A.TXT', '"C:\\\\A.TXT"');
+testJSON('C:\\B.TXT', '"C:\\\\B.TXT"');
+testJSON(['C:\\A.TXT','C:\\B.TXT','C:\\C.TXT','C:\\D.TXT'], '["C:\\\\A.TXT","C:\\\\B.TXT","C:\\\\C.TXT","C:\\\\D.TXT"]');
+testJSON({'dDefault': '1.84467440737E+19'}, '{"dDefault":"1.84467440737E+19"}');
 testJSON([undefined, undefined, 1, 2], '[null,null,1,2]');
 testJSON([0, false, function() {}], '[0,false,null]');
 testJSON(0, '0');
@@ -67,25 +63,23 @@ testJSON(new Date(2008, 9, 25), '"2008-10-25T05:00:00.000Z"');
 // Temporarily remove Date's toJSON and JSON.stringify
 dateToJSON = Date.prototype.toJSON;
 Date.prototype.toJSON = null;
-if (typeof(JSON) == 'object' && JSON.stringify)
-{
-    jsonStringify = JSON.stringify
-    JSON.stringify = null;
+if (typeof JSON === 'object' && JSON.stringify) {
+	jsonStringify = JSON.stringify;
+	JSON.stringify = null;
 }
 
 testJSON(new Date(2008, 9, 25), '"2008-10-25T05:00:00.000Z"');
 
 // Restore Date's toJSON and JSON.stringify
 Date.prototype.toJSON = dateToJSON;
-if (typeof(jsonStringify) != 'undefined')
-    JSON.stringify = jsonStringify;
+if (typeof jsonStringify !== 'undefined')
+	JSON.stringify = jsonStringify;
 
-console.log("Done.")
+console.log('Done.');
 
-$(function()
-{
-    if (tests_fail > 0)
-        $('h1 span').addClass('fail').empty().append("FAIL (" + tests_fail +")");
-    else
-        $('h1 span').addClass('passed').empty().append("PASSED");
-})
+$(function() {
+	if (tests_fail > 0)
+		$('h1 span').addClass('fail').empty().append('FAIL (' + tests_fail +')');
+	else
+		$('h1 span').addClass('passed').empty().append('PASSED');
+});
